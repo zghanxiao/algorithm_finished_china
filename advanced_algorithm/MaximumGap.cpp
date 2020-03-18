d88bb308effa2b75c7784125bc6fb4426afd21be
xiao han <sy_hanxiao2@sina.com>
2020-03-18 11:23:02 +0800

    MaximumGap.cpp accepted

--- advanced_algorithm/MaximumGap.cpp
+++ advanced_algorithm/MaximumGap.cpp
@@ -4,17 +4,21 @@ public:
      * @param nums: an array of integers
      * @return: the maximun difference
      */
+    bool valid(long long val) {
+      return val != LLONG_MIN && val != LLONG_MAX;
+    }
+    
     int maximumGap(vector<int> &nums) {
        // write your code here
-       
       long long size = nums.size();
+      
       if (size < 2) {
         return 0;
       }
+      
       long long max_val = LLONG_MIN;
       long long min_val = LLONG_MAX;
       
-       
       for (long long iter = 0; iter < size; ++ iter) {
         long long num = nums[iter]; 
         max_val = max(max_val, num);
@@ -22,24 +26,40 @@ public:
       }
       
       long long avg_interval = (max_val - min_val + size  - 2) / (size - 1);
+      if (!avg_interval) {
+        return 0;
+      } 
       
-      vector<long long> min_arr(size - 1, LLONG_MAX);
-      vector<long long> max_arr(size - 1, LLONG_MIN); 
+      vector<long long> min_arr(size, LLONG_MAX);
+      vector<long long> max_arr(size, LLONG_MIN); 
       
       for (long long iter = 0; iter < size; ++ iter) {
         long long num = nums[iter];
         long long num_of_avg = (num - min_val) / avg_interval;
-        if (num != min_val) {
+        if (num != min_val && num != max_val) {
           min_arr[num_of_avg] = min(min_arr[num_of_avg], num);
           max_arr[num_of_avg] = max(max_arr[num_of_avg], num);
         }
       } 
       
       long long max_diff = LLONG_MIN;
-      for (long long iter = 1; iter < size - 1; ++ iter ) {
-        long long diff  = min_arr[iter] - max_arr[iter - 1];
-        max_diff = max(max_diff, diff);
+      long long last_max = min_val;
+      for (long long iter = 0; iter < size; ++ iter ) {
+        long long diff;
+        
+        if (iter != 0 && valid(min_arr[iter])) {
+          diff  =  min_arr[iter] - last_max;
+          max_diff = max(max_diff, diff);
+        } 
+        if (valid(max_arr[iter])) {
+          last_max = max(max_arr[iter], last_max);
+        }
       }
+      if (valid(last_max)) {
+        long long diff = max_val - last_max;
+        max_diff = max(max_diff, diff);
+      } 
+      
       return (int)max_diff;
     }
 };
