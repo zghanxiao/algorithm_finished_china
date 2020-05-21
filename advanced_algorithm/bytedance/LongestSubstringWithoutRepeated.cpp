62b874150765ee8d06b24efe090ef2e301de9f39
xiao han <sy_hanxiao2@sina.com>
2020-05-21 11:44:56 +0800

    pre_i do not inc 1 step each time.

--- advanced_algorithm/bytedance/LongestSubstringWithoutRepeated.cpp
+++ advanced_algorithm/bytedance/LongestSubstringWithoutRepeated.cpp
@@ -1,58 +1,27 @@
 class Solution {
 public:
-  int inc_map(unordered_map<int, int>& cnt, int key) {
-    if (cnt.find(key) == cnt.end()) {
-      cnt[key] = 0;
-    }
-    cnt[key] ++;
-    return cnt[key];
-  }
-  
-  int dec_map(unordered_map<int, int>& cnt, int key) {
-    if (cnt.find(key) == cnt.end()) {
-      return -1;
-    }  
-    int ret_val = -- cnt[key]; 
-    if (!ret_val) {
-      cnt.erase(key);
-    }
-    return ret_val;
-  }
-  
-  bool repeat(int str_i, int pre_i, int map_size) {
-    return str_i - pre_i + 1 > map_size;
-  }
   
   int lengthOfLongestSubstring(string s) {
-    unordered_map<int, int> cnt;
-    int str_i = -1;
+    unordered_map<char, int> cnt;
+    int str_i = 0;
     int pre_i = 0;
     int longest = 0;
     int size = s.size();
-    while(true) {
-      while(true) {
-        ++ str_i;
+    int break_flag = false;
+    for(; str_i < size ; ++ str_i ) {
         
-        if (str_i >= size) {
-          break;  
-        }
-        
-        char ch = s[str_i];    
-        if(inc_map(cnt, ch) > 1) {
-          break;  
+      char ch = s[str_i];    
+      if (cnt.find(ch) != cnt.end()) {
+        int dup_i = cnt[ch];
+        if (dup_i >= pre_i) {
+          longest = max(longest, str_i - pre_i);  
+          pre_i = dup_i + 1;   
         }
       }
-      
-      longest = max(longest, str_i - pre_i);  
-      if(str_i == size){
-        break;
-      }
-      
-      for(;repeat(str_i, pre_i, cnt.size()); ++ pre_i) {
-        char ch = s[pre_i];    
-        dec_map(cnt, ch);
-      }
+      cnt[ch] = str_i;
     }
+      
+    longest = max(longest, str_i - pre_i);  
     return longest;
   }
 };
