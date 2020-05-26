c391a14cdaed24a15ba355b2d8aad2d0ba38e1ed
xiao han <sy_hanxiao2@sina.com>
2020-05-26 17:00:29 +0800

    Add FindMedianFromDataStream.

---
+++ advanced_algorithm/bytedance/FindMedianFromDataStream.cpp
@@ -0,0 +1,47 @@
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+  
+    priority_queue<int, vector<int>, greater<int> > min_h;
+    priority_queue<int, vector<int>> max_h;
+  
+    MedianFinder() {
+      while(!max_h.empty()){
+        max_h.pop();
+      }   
+      while(!min_h.empty()) {
+        min_h.pop();
+      }
+    }
+    
+    // maitain min_h.size() == max_h.size() || min_h.size() + 1 == max_h.size()
+    void addNum(int num) {
+      if (min_h.size() == max_h.size()) {
+        if (!min_h.empty() && num > min_h.top()) {
+          min_h.push(num);
+          num = min_h.top();
+          min_h.pop();
+        }
+        max_h.push(num);
+      } else {
+        if (num < max_h.top()) {
+          max_h.push(num);
+          num = max_h.top();
+          max_h.pop();
+        }
+        min_h.push(num);
+      } 
+    }
+    
+    double findMedian() {
+      if (max_h.size() == 0) {
+        return 0.0;
+      }
+      
+      if (max_h.size() > min_h.size()) {
+        return (double)(max_h.top());
+      } else {
+        return ((double)(max_h.top()) + (double)(min_h.top())) / 2.0;
+      }    
+    }
+};
