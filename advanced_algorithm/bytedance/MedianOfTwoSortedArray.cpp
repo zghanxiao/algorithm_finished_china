5a12c6b3caa3515d3cdd6a400f07dd128eba3100
xiao han <sy_hanxiao2@sina.com>
2020-05-28 17:26:55 +0800

    Add MedianOfTwoSortedArray

--- advanced_algorithm/bytedance/MedianOfTwoSortedArray.cpp
+++ advanced_algorithm/bytedance/MedianOfTwoSortedArray.cpp
@@ -1,66 +1,76 @@
 class Solution {
 public:
+  
+    bool qEmpty(vector<int> & nums, int i) {
+      return i == nums.size();  
+    }
+  
+    int qSize(vector<int> & nums, int i) {
+      return nums.size() - i;
+    }
+  
+    int qTop(vector<int> & nums, int i) {
+      return nums[i];  
+    }
+  
+    bool qPopLen(vector<int> & nums, int &i, int len) {
+      if(i + len > nums.size()){
+        return false; 
+      }   
+      i += len;
+      return true;
+    }
+  
+    int qIndex(vector<int> &nums, int i, int new_i) {
+      return nums[i + new_i];  
+    }
+  
+    int findkth(vector<int>& nums1, vector<int> & nums2, int k, int i1, int i2) {
+      while(k) {
+        int q_size1 = qSize(nums1, i1);
+        int q_size2 = qSize(nums2, i2);
+        
+        if (k > q_size1 + q_size2) {
+          return -1;
+        }
+        
+        if(q_size1 > q_size2) {
+          swap(nums1, nums2);
+          swap(i1, i2);
+          swap(q_size1, q_size2);
+        }
+        
+        if (k == 1) {
+          int q_val1 = qEmpty(nums1, i1) ? INT_MAX:qTop(nums1, i1);
+          int q_val2 = qTop(nums2, i2);
+          return min(q_val1, q_val2);
+        } 
+        
+        int cut_len1 = min(q_size1, k/2) , cut_len2 = k - cut_len1;
+        
+        if (cut_len1 == 0) {
+          return qIndex(nums2, i2, cut_len2 - 1);  
+        } else if (qIndex(nums1, i1, cut_len1 - 1) < qIndex(nums2, i2, cut_len2 - 1)) {
+          qPopLen(nums1, i1, cut_len1); 
+          k -= cut_len1;
+        } else {
+          qPopLen(nums2, i2, cut_len2);
+          k -= cut_len2;
+        }
+      }
+      return -1;
+    }
+  
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-      vector<vector<int> > matrix(2);
-      matrix.push_back(nums1);
-      matrix.push_back(nums2);
-      
-      priority_queue<pair<int, pair<int, int> >,
-                     vector<pair<int, pair<int, int> > >,
-                     greater<pair<int, pair<int,int> > > > min_heap;
-      
-      int tot_num = 0;
-      if (nums1.size() > 0) {
-        min_heap.push(matrix[0][0], make_pair(0, 0));
-        tot_num += nums1.size();
+      int tot_size = nums1.size() + nums2.size();
+      double res = 0.0;
+      if (tot_size % 2 == 0) {
+        int res1 = findkth(nums1, nums2, tot_size/2, 0, 0); 
+        int res2 = findkth(nums1, nums2, tot_size/2 + 1, 0, 0); 
+        res = ((double)(res1) + (double)(res2))/2.0;
+      } else if (tot_size % 2 == 1) {
+        res = (double)(findkth(nums1, nums2, (tot_size + 1)/2, 0, 0)); 
       }
-      
-      if (nums2.size() > 0) {
-        min_heap.push(matrix[1][0], make_pair(1, 0));
-        tot_num += nums2.size();
-      }
-      
-      int cnt = 0;
-      unordered_set<int> median_cnt;
-      vector<int> values;
-      int max_i = 0;
-      if (tot_sum %2 == 0) {
-        median_cnt.insert(tot_sum / 2);  
-        median_cnt.insert(tot_sum / 2 + 1);
-        max_i = tot_sum / 2 + 1;
-      } else {
-        median_cnt.insert((tot_sum + 1) / 2);
-        max_i = (tot_sum + 1) / 2;
-      }
-      
-      while(min_heap.size()) {
-        ++ cnt;
-        pair<int, pair<int, int> > top = min_heap.top();
-        int val = top.first;
-        int g_r_i = top.second.first;
-        int g_c_i = top.second.second;
-        top.pop();
-        
-        if (cnt > max_i) {
-           break;
-        }
-        
-        if (median_cnt.find(cnt) != median_cnt.end()) {
-          values.push_back(val);    
-        }
-        
-        int g_r_i_size = matrix[g_r_i].size();
-        if (g_c_i + 1 < g_r_i_size) {
-          min_heap.push(matrix[g_r_i][g_c_i + 1], make_pair(g_r_i, g_c_i + 1));
-        }
-      }
-      
-      double median = 0;
-      if (values.size() == 1) {
-        median = values[0]; 
-      } else {
-        median = (values[0] + values[1]) / 2.0; 
-      }
-      return median; 
+      return res;
     }
 };
