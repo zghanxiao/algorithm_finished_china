8adfdf0d6fb864d345dd0031f32f4177235f3bc6
xiao han <sy_hanxiao2@sina.com>
2020-05-31 23:00:19 +0800

    Add MedianOfTwoSortedArray

--- advanced_algorithm/bytedance/MedianOfTwoSortedArray.cpp
+++ advanced_algorithm/bytedance/MedianOfTwoSortedArray.cpp
@@ -1,61 +1,49 @@
 class Solution {
 public:
   
-    bool qEmpty(vector<int> & nums, int i) {
-      return i == nums.size();  
+    bool qEmpty(pair<vector<int>, int> &q) {
+      return q.first.size() == q.second;
     }
   
-    int qSize(vector<int> & nums, int i) {
-      return nums.size() - i;
+    int qSize(pair<vector<int>, int> &q) {
+      return q.first.size() - q.second;
     }
   
-    int qTop(vector<int> & nums, int i) {
-      return nums[i];  
+    int qTop(pair<vector<int>, int> &q) {
+      return q.first[q.second];  
     }
   
-    bool qPopLen(vector<int> & nums, int &i, int len) {
-      if(i + len > nums.size()){
+    bool qPopLen(pair<vector<int>, int> &q, int len, int & k) {
+      if(q.second + len > q.first.size()){
         return false; 
       }   
-      i += len;
+      q.second += len;
+      k -= len;
       return true;
     }
   
-    int qIndex(vector<int> &nums, int i, int new_i) {
-      return nums[i + new_i];  
+    int qIndex(pair<vector<int>, int> &q, int new_i) {
+      return q.first[q.second + new_i];  
     }
   
-    int findkth(vector<int>& nums1, vector<int> & nums2, int k, int i1, int i2) {
-      while(k) {
-        int q_size1 = qSize(nums1, i1);
-        int q_size2 = qSize(nums2, i2);
+    int findkth(pair<vector<int>, int> &q1, pair<vector<int>, int> &q2,  int k) {
+      while(k <= qSize(q1) + qSize(q2)) {
         
-        if (k > q_size1 + q_size2) {
-          return -1;
+        if(qSize(q1)> qSize(q2)) {
+          swap(q1, q2);
         }
         
-        if(q_size1 > q_size2) {
-          swap(nums1, nums2);
-          swap(i1, i2);
-          swap(q_size1, q_size2);
-        }
-        
-        if (k == 1) {
-          int q_val1 = qEmpty(nums1, i1) ? INT_MAX:qTop(nums1, i1);
-          int q_val2 = qTop(nums2, i2);
-          return min(q_val1, q_val2);
+        if (qEmpty(q1)) {  
+          return qIndex(q2, k - 1);
+        } else if (k == 1) {
+          return min(qTop(q1), qTop(q2));
         } 
         
-        int cut_len1 = min(q_size1, k/2) , cut_len2 = k - cut_len1;
-        
-        if (cut_len1 == 0) {
-          return qIndex(nums2, i2, cut_len2 - 1);  
-        } else if (qIndex(nums1, i1, cut_len1 - 1) < qIndex(nums2, i2, cut_len2 - 1)) {
-          qPopLen(nums1, i1, cut_len1); 
-          k -= cut_len1;
+        int cut_len1 = min(qSize(q1), k/2) , cut_len2 = k - cut_len1;
+        if (qIndex(q1, cut_len1 - 1) < qIndex(q2, cut_len2 - 1)) {
+          qPopLen(q1, cut_len1, k); 
         } else {
-          qPopLen(nums2, i2, cut_len2);
-          k -= cut_len2;
+          qPopLen(q2, cut_len2, k);
         }
       }
       return -1;
@@ -64,12 +52,15 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
       int tot_size = nums1.size() + nums2.size();
       double res = 0.0;
+      pair<vector<int>, int> q1= make_pair(nums1, 0);
+      pair<vector<int>, int> q2= make_pair(nums2, 0);
       if (tot_size % 2 == 0) {
-        int res1 = findkth(nums1, nums2, tot_size/2, 0, 0); 
-        int res2 = findkth(nums1, nums2, tot_size/2 + 1, 0, 0); 
+        int res1 = findkth(q1, q2, tot_size/2); 
+        q1.second = q2.second = 0;
+        int res2 = findkth(q1, q2, tot_size/2 + 1); 
         res = ((double)(res1) + (double)(res2))/2.0;
       } else if (tot_size % 2 == 1) {
-        res = (double)(findkth(nums1, nums2, (tot_size + 1)/2, 0, 0)); 
+        res = (double)(findkth(q1, q2, (tot_size + 1) /2)); 
       }
       return res;
     }
